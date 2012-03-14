ec8be6d6cdb6e905e488b626ceab8b500004b984
ftrcn <fatma.tercan@bil.omu.edu.tr>
2012-03-14 23:09:15 +0200

    .

---
+++ föyler/yariLar.topLami.py
@@ -0,0 +1,16 @@
+from __future__ import division
+
+def yarilar_toplami(sayi):
+    a=0
+    if sayi>0:
+    
+        for i in  range(sayi):
+
+            sayi=sayi/2
+            a=a+sayi
+
+        print a
+            
+            
+    else:
+        print "lütfen pozitif bir sayý giriniz."
