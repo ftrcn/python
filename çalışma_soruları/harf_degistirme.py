06c8eb8b494e0e653c01d073382b3d3bb119493e
ftrcn <fatma.tercan@bil.omu.edu.tr>
2012-04-19 03:34:55 +0300

    .

--- çalışma_soruları/harf_degistirme.py
+++ çalışma_soruları/harf_degistirme.py
@@ -1,13 +1,13 @@
 # -*- coding: cp1254 -*-
 #!/usr/bin/python
-def dizgi_harf_degistir(dizgi,degistirilecek_harfin_sirasi,harf):
+def dizgi_harf_degistir(string,harfin_sirasi,harf):
     a=""
-    c=0
+    b=0
     harf=str(harf)
-    dizgi=str(dizgi)
-    for i in dizgi :
-        c+=1
-        if c==degistirilecek_harfin_sirasi:
+    string=str(string)
+    for i in string:
+        b+=1
+        if b==harfin_sirasi:
             a+=harf
         else:
             a+=i
